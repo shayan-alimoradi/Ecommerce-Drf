00a4686ae71fb39bf5752b6d830d5c338f985cdf
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-05-08 18:56:12 +0430

    refactor: no-self-use in change-password-serializer method

--- account/api/serializers.py
+++ account/api/serializers.py
@@ -106,6 +106,7 @@ class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
 
-    def validate_new_password(self, value):
+    @staticmethod
+    def validate_new_password(value):
         validate_password(value)
         return value
