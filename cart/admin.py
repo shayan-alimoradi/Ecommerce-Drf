e792b02be5515a58b61fdb4df9356dd07b5d9e37
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-05-08 19:07:36 +0430

    feat(cart): Add models to admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,14 @@
+# Core django imports
 from django.contrib import admin
 
-# Register your models here.
+# Local imports
+from .models import Cart, CartItem
+
+
+@admin.register(CartItem)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("cart", "product", "quantity", "id")
+    search_fields = ("cart", "product")
+
+
+admin.site.register(Cart)
