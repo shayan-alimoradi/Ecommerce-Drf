3743e6436e2766e444164d7af98556086e4c6438
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-05-10 22:54:50 +0430

    feat(comment): Add model to admin panel

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "content_type",
+        "object_id",
+        "content_object",
+        "id",
+    )
