91d586d869be231ca3108d7d05cb90079614cd8a
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-05-10 16:01:17 +0430

    feat(order): Add models to admin panel

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem, Coupon
+
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ("product", "order", "quantity")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "created", "is_paid", "id")
+    list_filter = ("is_paid",)
+
+    inlines = (OrderItemAdmin,)
+
+
+admin.site.register(OrderItem)
+admin.site.register(Coupon)
